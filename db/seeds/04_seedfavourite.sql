9c5d3aecc46f80b28a1a8952dc3c5f4d10487c6e
jang walia <jangiwalia83@gmail.com>
2022-02-01 13:00:02 -0800

    write a query to seed favourite_maps table

---
+++ db/seeds/04_seedfavourite.sql
@@ -0,0 +1,6 @@
+INSERT INTO favourite_maps (user_id,map_id,created_at,removed_at)
+VALUES  (9,2,'2021-12-12',NULL);
+INSERT INTO favourite_maps (user_id,map_id,created_at,removed_at)
+VALUES  (7,1,'2021-12-15',NULL);
+INSERT INTO favourite_maps (user_id,map_id,created_at,removed_at)
+VALUES  (9,1,'2021-12-22',NULL);
