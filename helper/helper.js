ca84d4fe591e8a5e75df418ff19e884e98afa1ce
Anjali Soni <anjalipatadiya@gmail.com>
2022-02-02 14:58:05 -0700

    Create helper function here

---
+++ helper/helper.js
@@ -0,0 +1,23 @@
+//Create function to get user by email
+
+/* const getUserByEmail = function (db,email) {
+  for(const key in db){
+    if(db[key].email === email){
+      return db[key];
+   }
+  }
+  return undefined;
+}; */
+
+/* const getuserByID = (db,id)=>{
+  return db.query (`SELECT * FROM users
+                    WHERE id = ${id};`)
+        .then(user => {
+          // return user.rows;
+          console.log("testing", user.rows);
+        })
+};
+
+module.exports = {getuserByID}; */
+
+
