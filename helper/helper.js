24ef7ebe5f3ef4f60c55622c8dce12ccf4ef79a5
jang walia <jangiwalia83@gmail.com>
2022-02-03 06:52:36 -0800

    add more helper functions

--- helper/helper.js
+++ helper/helper.js
@@ -9,15 +9,33 @@
   return undefined;
 }; */
 
-/* const getuserByID = (db,id)=>{
-  return db.query (`SELECT * FROM users
-                    WHERE id = ${id};`)
-        .then(user => {
-          // return user.rows;
-          console.log("testing", user.rows);
+const getuserByID = (db,givenId)=>{
+  const queryString = `SELECT * FROM users
+  WHERE id = $1;`
+  const values = [givenId];
+  return db.query (queryString,values)
+        .then(res => {
+         return  res.rows[0];
+        })
+        .catch(err =>{
+          return undefined;
         })
 };
 
-module.exports = {getuserByID}; */
+const getAllMaps = (db) =>{
+  const queryString = `SELECT maps.title as map ,maps.id as id , users.name as created_by
+  FROM maps
+  JOIN users ON users.id = user_id;`
+  return db.query(queryString)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err =>{
+      return undefined;
+    })
+}
+
+
+module.exports = {getuserByID,getAllMaps};
 
 
