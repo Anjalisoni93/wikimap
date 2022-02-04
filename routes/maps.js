d548d41d6caa00d559b884ca335cb08d3a2aaa0d
jang walia <jangiwalia83@gmail.com>
2022-02-04 12:36:06 -0800

    refactor map.js

--- routes/maps.js
+++ routes/maps.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const {getuserByID,getAllMaps} = require('../helper/helper');
+const {getuserByID,getAllMaps,getMapById} = require('../helper/helper');
 
 module.exports = (db) => {
   //this route is to show all maps
@@ -13,15 +13,30 @@ module.exports = (db) => {
           getuserByID(db,id)
             .then(user =>{
             tempVariable.user = user
-            res.render('showMaps',tempVariable);
+            res.render('mapIndex',tempVariable);
           })
         })
-
-        // res.json(showMaps[0]);
       })
 
     //route to get specific map with id
+      router.get('/:id',(req,res)=>{
+        const id = req.params.id;
+        const tempVariable = {}
+        if(!req.session.user_id){
+          tempVariable.user = null;
+        }else{
+          getuserByID(db,req.session.user_id)
+          .then(user =>{
+            tempVariable.user = user
+          });
+        }
+        getMapById(db,id)
+        .then(mapDetails =>{
+          tempVariable.mapDetails = mapDetails
+         return  res.render('mapShow',tempVariable);
+        })
 
+      })
   return router;
 };
 
